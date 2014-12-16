f1aa8b1c8f681466735ebe0521c858acad0e3e4f
Oli Gustafsson <oli.gustafsson@gmail.com>
2014-12-16 14:24:31 +0100

    Grunt bumb

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,11 +37,28 @@ module.exports = function(grunt) {
             unit: {
                 configFile: 'karma.conf.js'
             }
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-bump');
     grunt.registerTask('default', ['jshint', 'karma','uglify']);
 };
